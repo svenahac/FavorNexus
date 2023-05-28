8d32c68335f8f06d9848b9ed72e0e20fc5a5e0ba
svenahac <ahacsh@gmail.com>
2023-05-28 07:33:45 +0200

    finished profile

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,29 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 font-medium text-slate-700">
       <a
-        href="#"
-        className="block md:inline-block px-3 py-2 rounded-md text-white focus:outline-none focus:text-white "
+        href="/home"
+        className="active block md:inline-block px-3 py-2 rounded-md hover:text-primaryGreen focus:outline-none focus:text-primaryGreen "
       >
         Home
       </a>
       <a
         href="#"
-        className="block md:inline-block px-3 py-2 rounded-md hover:text-white focus:outline-none focus:text-white"
+        className="block md:inline-block px-3 py-2 rounded-md hover:text-primaryGreen focus:outline-none focus:text-primaryGreen"
       >
         Leaderboard
       </a>
       <a
         href="#"
-        className="block md:inline-block px-3 py-2 rounded-md hover:text-white  focus:outline-none focus:text-white "
+        className="block md:inline-block px-3 py-2 rounded-md hover:text-primaryGreen  focus:outline-none focus:text-primaryGreen "
       >
-        Achievments
+        My Favors
       </a>
       <a
-        href="#"
-        className="block md:inline-block px-3 py-2 rounded-md hover:text-white focus:outline-none focus:text-white "
+        href="/profile"
+        className="block md:inline-block px-3 py-2 rounded-md hover:text-primaryGreen focus:outline-none focus:text-primaryGreen "
       >
         Profile
       </a>
